5db8a79925a867528c9dbc929d2346802299e49f
Max Sandoval <68491989+morpheomax@users.noreply.github.com>
2023-12-18 00:05:20 -0300

    PAD HTML - JS - CSS

---
+++ sounds.js
@@ -0,0 +1,22 @@
+import kick from './sounds/Kick_Trap.wav'
+import snare from './sounds/Snare_Low-fi_Trap.wav'
+import hithat from './sounds/Hithat_Trap.wav';
+import clap from './sounds/Clap_Trap.wav'
+import kickbass from './sounds/808_Kick_Bass.wav'
+import snarefill from './sounds/Snare_Fill.wav'
+import fxstart from './sounds/FX_Start_Up.wav'
+import ridelong from './sounds/Ride_808_Long_Soft.wav'
+import synth from './sounds/Synth_Drama_062_D3.wav'
+
+export {
+    kick,
+    snare,
+    hithat,
+    clap,
+    kickbass,
+    snarefill,
+    fxstart,
+    ridelong,
+    synth
+}
+
